6235b23ce216db9f879bfcf69a64e5f2887b339f
LAPTOP-PQ1RE77H\rajib <rajibbasu28@gmail.com>
2022-09-20 21:24:01 +0530

    Updated connection string

--- PCIapi/Model/DBconnection.cs
+++ PCIapi/Model/DBconnection.cs
@@ -1,6 +1,8 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace PCIapi.Model
 {
@@ -10,14 +12,14 @@ namespace PCIapi.Model
         public DBconnection()
         {
             connectionString = @"Persist Security Info=False;User ID=pcidb;password=pc1@DB@#!!;Initial Catalog=pciDB; Data Source=pciproject.database.windows.net;Connection Timeout=100000;";
-                
-                
-                
-                //            IConfigurationRoot configuration = new ConfigurationBuilder()
-            //.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-            //.AddJsonFile("appsettings.json")
-            //.Build();
-            //            connectionString = configuration.GetConnectionString("PCIDBconnection");
+
+
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            connectionString = configuration.GetConnectionString("PCIDBconnection");
 
         }
 
--- PCIapi/Program.cs
+++ PCIapi/Program.cs
@@ -1,3 +1,6 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +21,21 @@ namespace PCIapi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfiguration = config.Build();
+
+                string kvURL = builtConfiguration["KeyVaultConfig:kvUrl"];
+                string tenantId = builtConfiguration["KeyVaultConfig:TenantId"];
+                string clientId = builtConfiguration["KeyVaultConfig:ClientId"];
+               
+                string ClientSecret = builtConfiguration["KeyVaultConfig:ClientSecretId"];
+
+                var credential = new ClientSecretCredential(tenantId, clientId, ClientSecret);
+
+                var client = new SecretClient(new Uri(kvURL), credential);
+                config.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
