761a330aabd383bbef4a3fd5a52ee9bbdef3db10
LAPTOP-PQ1RE77H\rajib <rajibbasu28@gmail.com>
2022-09-20 11:51:49 +0530

    Added Model and Controller for PciCmp

---
+++ PCIapi/Controllers/ManagePciCmpController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCIapi.Model;
+using System.Collections.Generic;
+
+namespace PCIapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagePciCmpController : Controller
+    {
+        private readonly ManagePciCmp managePciCmp;
+        public ManagePciCmpController()
+        {
+            managePciCmp = new ManagePciCmp();
+        }
+
+        [HttpGet]
+        public IEnumerable<pciCmp> get()
+        {
+            return managePciCmp.getPciDetails();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<pciCmp> get(int id)
+        {
+            return managePciCmp.getPciDetails(id);
+        }
+    }
+}
