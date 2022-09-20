ab2351a66ae079f3d99fc7572b79ee33e4ba50af
LAPTOP-PQ1RE77H\rajib <rajibbasu28@gmail.com>
2022-09-20 10:33:50 +0530

    Removed local connection string

--- PCIapi/Model/DBconnection.cs
+++ PCIapi/Model/DBconnection.cs
@@ -8,7 +8,6 @@ namespace PCIapi.Model
         private string connectionString;
         public DBconnection()
         {
-            //connectionString = @"Persist Security Info=False;User ID=sa;password=sa;Initial Catalog=PYT; Data Source=RAJIBBASU-PC\SQLEXPRESS;Connection Timeout=100000;";
             connectionString = @"Persist Security Info=False;User ID=pcidb;password=pc1@DB@#!!;Initial Catalog=pciDB; Data Source=pciproject.database.windows.net;Connection Timeout=100000;";
 
         }
