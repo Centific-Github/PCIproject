770bddd267c5c2cf8de191f35f3255ae719faba9
Pusala Monisree Sai Raji <monisree.pusala@pacteraedge.com>
2022-09-20 16:14:41 +0530

    Added Model andController for KeyAreas

---
+++ PCIapi/Controllers/ManageKeyAreasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCIapi.Model;
+using System.Collections.Generic;
+
+namespace PCIapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageKeyAreasController : Controller
+    {
+        ManageKeyAreas oManageKeyAreas = new ManageKeyAreas();
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IEnumerable<keyAreas> get()
+        {
+            return oManageKeyAreas.getKeyAreaDeatails();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<keyAreas> get(int id)
+        {
+            return oManageKeyAreas.getKeyAreaDeatails(id);
+        }
+
+
+    }
+}
