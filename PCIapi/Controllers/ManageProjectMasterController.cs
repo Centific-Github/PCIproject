f22d22710fe6c90a20343305c38e14036f9559d6
Hema sri <hemasri.duvvuru@P0191367.com>
2022-09-21 12:08:24 +0530

    Added model and controller for ProjectMaster

---
+++ PCIapi/Controllers/ManageProjectMasterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCIapi.Model;
+using System.Collections.Generic;
+
+namespace PCIapi.Controllers
+{
+    /// <summary>
+    /// This code is written by D.Hemasri
+    /// Date-20-09-2022
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageProjectMasterController : Controller
+    {
+        ManageProjectMaster oManageProjectMaster = new ManageProjectMaster();
+
+        [HttpGet]
+        public IEnumerable<projectMaster> get()
+        {
+            return oManageProjectMaster.getProjectDetails();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<projectMaster> get(int id)
+        {
+            return oManageProjectMaster.getProjectDetails(id);
+        }
+
+
+
+    }
+}
---
+++ PCIapi/Model/ManageProjectMaster.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PCIapi.Model
+
+{
+    public class ManageProjectMaster : DBconnection
+    {
+
+        public IEnumerable<projectMaster> getProjectDetails(string manager)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT ProjectID ,ProjectCode,ProjectManager FROM MstProjectMaster ";
+                dbConnection.Open();
+                return dbConnection.Query<projectMaster>(sQuery);
+            }
+        }
+
+        internal IEnumerable<projectMaster> getProjectDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<projectMaster> getProjectDetails(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT ProjectID ,ProjectCode,ProjectManager FROM MstProjectMaster WHERE ProjectID=@_Projectid ";
+                dbConnection.Open();
+                return dbConnection.Query<projectMaster>(sQuery, new { _Projectid = id });
+            }
+        }
+        public IEnumerable<projectMaster> getProjectDetails(projectMaster _projectMaster)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT ProjectID ,ProjectCode,ProjectManager FROM MstProjectMaster WHERE ProjectCode=@_strProjectCode AND ProjectManager=@_strProjectManager ";
+                dbConnection.Open();
+                return dbConnection.Query<projectMaster>(sQuery, new { _strProjectCode = _projectMaster.ProjectCode, _strProjectManager = _projectMaster.ProjectManager });
+            }
+        }
+    }
+
+
+    public class projectMaster
+
+    {
+
+        public int ProjectID { get; set; }
+        public string ProjectCode { get; set; }
+        public string ProjectManager { get; set; }
+    }
+}
+
