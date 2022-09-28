d04dfd2d5d137513991914e38edb2519dd77a0fc
Jorepalli Sumalatha <sumalatha.jorepalli@pacteraedge.com>
2022-09-28 13:36:11 +0530

    Add SubActivityMaster model and controller.

---
+++ PCIapi/Model/ManageSubActivityMaster.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PCIapi.Model
+{
+    public class ManageSubActivityMaster : DBconnection
+    {
+        public ManageSubActivityMaster(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+        public IEnumerable<subActivityMaster> getsubActivityDetails()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT  SubHeadingID,AreasID,HeadingID,keyDescription  FROM MstSubActivityMaster";
+                dbConnection.Open();
+                return dbConnection.Query<subActivityMaster>(sQuery);
+            }
+        }
+        public IEnumerable<subActivityMaster> getsubActivityDetails(int areasId, int headingId)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT SubHeadingID,AreasID,HeadingID,keyDescription FROM MstSubActivityMaster WHERE AreasID=@_areasId AND HeadingID=@_HeadingID";
+                dbConnection.Open();
+                return dbConnection.Query<subActivityMaster>(sQuery, new { _areasId= areasId, _HeadingID = headingId });
+            }
+        }
+    }
+
+    public class subActivityMaster
+    {
+        public int SubHeadingID{ get; set; }
+        public int AreasID { get; set; }
+        public int HeadingID { get; set; }
+        public string keyDescription { get; set; }
+    }
+}
+
+   
