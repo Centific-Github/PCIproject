8a1922b28124e61f7ec936c5fc2f2e8a7468a62a
Velpula Pushpa <pushpa.velpula@pacteraedge.com>
2022-09-29 11:29:51 +0530

    Add model and constructor for ManageScore

---
+++ PCIapi/Controllers/ManageScoreController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PCIapi.Model;
+using System.Collections.Generic;
+
+
+namespace PCIapi.Controllers
+/// <summary>
+/// following code has written by velpula Pushpa
+/// date 28/09/2022
+/// </summary>
+{
+    [Route("api/[controller]")]
+        [ApiController]
+    public class ManageScoreController : Controller
+    {
+        private IConfiguration _configuration;
+
+
+        private readonly ManageScore manageScoreController;
+        public ManageScoreController(IConfiguration configuration)            
+        {
+            _configuration = configuration;
+            manageScoreController = new ManageScore(_configuration);
+        }        
+        [HttpGet]
+            public IEnumerable<mstScore> get()
+            {
+                return manageScoreController.getMstScoreDetails();
+            }       
+
+        [HttpGet("{id}")]
+            public IEnumerable<mstScore> get(int id)
+            {
+            return manageScoreController.getMstScoreDetails(id);
+        }        
+    }
+ }
+
---
+++ PCIapi/Model/ManageScore.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PCIapi.Model
+/// <summary>
+/// following code has written by velpula Pushpa
+/// date 28/09/2022
+/// </summary>
+{
+    public class ManageScore : DBconnection
+    {
+        public ManageScore(IConfiguration configuration) : base(configuration)
+        {
+
+        }    
+
+        public IEnumerable<mstScore> getMstScoreDetails()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+
+                string sQuery = @"SELECT  ScoreCrdID, CeremID,AreasID,CompID,PcicmpID,HeadingID,ExcKeyActivityID,KeyActivitiesID,ActivityID,ScoreID,ScoreValue  from MstScore";
+                dbConnection.Open();
+                return dbConnection.Query<mstScore>(sQuery);
+            }
+        }
+        public IEnumerable<mstScore> getMstScoreDetails(int ID)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT  ScoreCrdID, CeremID,AreasID,CompID,PcicmpID,HeadingID,ExcKeyActivityID,KeyActivitiesID,ActivityID,ScoreID,ScoreValue  from MstScore Where ScoreCrdID=@_strScoreCrdID";
+                dbConnection.Open();
+                return dbConnection.Query<mstScore>(sQuery, new { _strScoreCrdID = ID});
+            }
+        }
+
+    }    
+        public class mstScore
+        {
+            public int ScoreCrdID { get; set; }
+            public int CeremID { get; set; }
+            public int AreasID { get; set; }
+            public int CompID { get; set; }
+            public int PcicmpID { get; set; }
+            public int HeadingID { get; set; }
+            public int ExcKeyActivityID {get; set; }
+            public int KeyActivitiesID { get; set; }
+            public int ActivityID { get; set; }
+            public int ScoreID { get; set; }
+            public int ScoreValue { get; set; }
+        }
+}
+
