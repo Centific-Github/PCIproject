c68c28b3c4aed29d217b9a54e4fb7395d57ac82a
Velpula Pushpa <pushpa.velpula@pacteraedge.com>
2022-10-04 10:48:09 +0530

    Added model and controller for managelogin

--- PCIapi/Model/ManageLogin.cs
+++ PCIapi/Model/ManageLogin.cs
@@ -15,7 +15,7 @@ namespace PCIapi.Model
     public class ManageLogin : DBconnection
     {
         private IConfiguration _configuration;
-        public ManageLogin(IConfiguration configuration) : base(configuration)
+public ManageLogin(IConfiguration configuration) : base(configuration)
         {
         _configuration = configuration;
         }
@@ -41,10 +41,17 @@ namespace PCIapi.Model
                 dbConnection.Open();
                 var affectedRows = dbConnection.Execute(sQuery, new { _strEmailID = emailID, _strPassword = password });
                 return affectedRows;
-            }
-            
+            }      
 
-           
+        }
+        public string getUserByEmailId(string emailID)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT UserName from MstLogintbl where EmailId=@_StrEmailId";
+                dbConnection.Open();
+                return dbConnection.Query<string>(sQuery, new { _StrEmailId = emailID }).FirstOrDefault();
+            }
 
         }
         public string UpdateSendPassword(string emailID)
@@ -53,9 +60,25 @@ namespace PCIapi.Model
             var roweffected = UpdateLoginPassword(emailID, password);
             if(roweffected >0)
             {
-                MailMessage mailMessage = new MailMessage(_configuration["Mail:EmailID"], "rahul.reddy@pacteraedge.com");
+                MailMessage mailMessage = new MailMessage(_configuration["Mail:EmailID"], emailID);
                 mailMessage.Subject = "Reset Password";
-                mailMessage.Body = "Your Random 8 Digit Password Is :"+password+"";
+                mailMessage.IsBodyHtml = true;
+                string emailbody = "<html>" +
+                   "<head>" +
+                   "<title>HTML email template</title>" +
+                   "<meta name=\"viewport\" content=\"width = 375, initial-scale = -1\">" +
+                   "</head>" +
+                   " <body style=\"background-color: #ffffff; font-size: 16px;\">" +
+                   "<center>" +
+                   "      <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"height:100%; width:600px;\">" +
+                   "<tr>" +
+                   " <td align=\"center\" bgcolor=\"#ffffff\" style=\"padding:30px\">" +
+                   "<p style=\"text-align:left\">Hi " + getUserByEmailId(emailID) + ", <br><br> We received a request to reset the password for your account for this email address. To initiate the password reset process for your account, click the link below.\r\n </p>" +
+                   "<p>\r\n <a target=\"_blank\" style=\"text-decoration:none; background-color: black; border: black 1px solid; color: #fff; padding:10px 10px; display:block;\" href=\"https://google.com\">\r\n<strong>Reset Password</strong></a>\r\n</p>" +
+                   "  <p style=\"text-align:left\">Your requested 8 digit password is : " + password + "" +
+                   "<p style=\"text-align:left\">\r\nSincerely,<br>The Website Team\r\n</p>" +
+                   "</td>\r\n</tr>\r\n        </tbody>\r\n      </table>\r\n    </center>\r\n  </body>\r\n</html>";
+                mailMessage.Body = emailbody;
                 var smpt = new System.Net.Mail.SmtpClient("smtp.gmail.com")
                 {
                     Port = 587,
