6148149a42f278d842e8b2735ccb54a8e7422736
jyothi121999 <jyothi.annabathina@pacteraedge.com>
2022-11-30 13:15:42 +0530

    Updated

--- PCIapi/Model/ManageKeyAreas.cs
+++ PCIapi/Model/ManageKeyAreas.cs
@@ -23,7 +23,7 @@ namespace PCIapi.Model
             {
                 string sQuery = @"SELECT AreasID, AreasDesc FROM MstKeyAreas";
                 dbConnection.Open();
-                return dbConnection.Query<keyAreas>(sQuery);
+                return dbConnection.Query<keyAreas>(sQuery);             
             }
         }     
         public IEnumerable<keyAreas> getKeyAreaDeatails(int id)
