413ea33e20eae1534aa84f80d8c2674e901b2db6
Jorepalli Sumalatha <sumalatha.jorepalli@pacteraedge.com>
2022-12-16 11:30:55 +0530

    Changes made in Manageuser.

--- PCIapi/Model/DBconnection.cs
+++ PCIapi/Model/DBconnection.cs
@@ -37,15 +37,15 @@ namespace PCIapi.Model
        
     }
 
-    //public class Settings
-    //{
-    //    public string AppName { get; set; }
-    //    public double Version { get; set; }
-    //    public long RefreshRate { get; set; }
-    //    public long FontSize { get; set; }
-    //    public string Language { get; set; }
-    //    public string Messages { get; set; }
-    //    public string BackgroundColor { get; set; }
-    //}
+    public class Settings
+    {
+        public string AppName { get; set; }
+        public double Version { get; set; }
+        public long RefreshRate { get; set; }
+        public long FontSize { get; set; }
+        public string Language { get; set; }
+        public string Messages { get; set; }
+        public string BackgroundColor { get; set; }
+    }
 
 }
