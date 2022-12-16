c125f3d65a1d09593cc9eee516e6021230e6d714
Jorepalli Sumalatha <sumalatha.jorepalli@pacteraedge.com>
2022-12-16 11:53:59 +0530

    Removed contact details in manageuser.

--- PCIapi/Model/ManageUsers.cs
+++ PCIapi/Model/ManageUsers.cs
@@ -35,7 +35,7 @@ namespace PCIapi.Model
         {
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = @"SELECT LoginID, EmailID, Password, UserName,EmployeeID,IsAdmin,FirstName,LastName,IsBlocked,GroupName,BusinessUnit,Account,ContactDetails from MstLogintbl";
+                string sQuery = @"SELECT LoginID, EmailID, Password, UserName,EmployeeID,IsAdmin,FirstName,LastName,IsBlocked,GroupName,BusinessUnit,Account from MstLogintbl";
                 dbConnection.Open();
                 return dbConnection.Query<userType>(sQuery);
             }
@@ -44,7 +44,7 @@ namespace PCIapi.Model
         {
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = @"SELECT LoginID, EmailID, Password, UserName,EmployeeID,IsAdmin,FirstName,LastName,IsBlocked,GroupName,BusinessUnit,Account,ContactDetails from MstLogintbl where LoginID=@_LoginId";
+                string sQuery = @"SELECT LoginID, EmailID, Password, UserName,EmployeeID,IsAdmin,FirstName,LastName,IsBlocked,GroupName,BusinessUnit,Account from MstLogintbl where LoginID=@_LoginId";
                 dbConnection.Open();
                 return dbConnection.Query<userType>(sQuery, new { _LoginId = id }).FirstOrDefault();
             }
@@ -53,7 +53,7 @@ namespace PCIapi.Model
         {
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = @"SELECT LoginID, EmailID, UserName,EmployeeID ,IsAdmin ,GroupName,BusinessUnit,Account,ContactDetails from MstLogintbl Where EmailID=@_strEmailID AND Password=@_strPassword";
+                string sQuery = @"SELECT LoginID, EmailID, UserName,EmployeeID ,IsAdmin ,GroupName,BusinessUnit,Account from MstLogintbl Where EmailID=@_strEmailID AND Password=@_strPassword";
                 dbConnection.Open();
                 return dbConnection.Query<userType>(sQuery, new { _strEmailID = _userType.EmailID, _strPassword = _userType.Password }).FirstOrDefault();
             }
@@ -62,9 +62,9 @@ namespace PCIapi.Model
         {
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = @"INSERT into MstLogintbl ( EmailID, UserName,EmployeeID,Password,FirstName,LastName,GroupName,BusinessUnit,Account,ContactDetails) values(@_strEmailID,@_strUserName,@_strPassword,@_strFirsttName,@_strLastName,@_strEmployeeID,@_strGroupName,@_strBusinessUnit,@_strAccount,@_strContactDetails)";
+                string sQuery = @"INSERT into MstLogintbl ( EmailID, UserName,EmployeeID,Password,FirstName,LastName,GroupName,BusinessUnit,Account) values(@_strEmailID,@_strUserName,@_strPassword,@_strFirsttName,@_strLastName,@_strEmployeeID,@_strGroupName,@_strBusinessUnit,@_strAccount)";
                 dbConnection.Open();
-                var affectedRows = dbConnection.Execute(sQuery, new { _strEmailID = _userTypeDTO.EmailID, _strUserName = _userTypeDTO.UserName, _strEmployeeID= _userTypeDTO.EmployeeID,  _strPassword = _userTypeDTO.Password, _strFirsttName = _userTypeDTO.FirstName, _strLastName = _userTypeDTO.LastName, _strGroupName = _userTypeDTO.GroupName, _strBusinessUnit = _userTypeDTO.BusinessUnit, _strAccount = _userTypeDTO.Account, _strContactDetails = _userTypeDTO.ContactDetails });
+                var affectedRows = dbConnection.Execute(sQuery, new { _strEmailID = _userTypeDTO.EmailID, _strUserName = _userTypeDTO.UserName, _strEmployeeID= _userTypeDTO.EmployeeID,  _strPassword = _userTypeDTO.Password, _strFirsttName = _userTypeDTO.FirstName, _strLastName = _userTypeDTO.LastName, _strGroupName = _userTypeDTO.GroupName, _strBusinessUnit = _userTypeDTO.BusinessUnit, _strAccount = _userTypeDTO.Account });
                  if (affectedRows > 0)
                 {
                     WelcomeMail(_userTypeDTO);
@@ -231,7 +231,6 @@ namespace PCIapi.Model
             public string? GroupName { get; set; }
             public string? BusinessUnit { get; set; }
             public string? Account { get; set; }
-            public int? ContactDetails { get; set; }
 
 
 
@@ -249,7 +248,6 @@ namespace PCIapi.Model
             public string? GroupName { get; set; }
             public string? BusinessUnit { get; set; }
             public string? Account { get; set; }
-            public int? ContactDetails { get; set; }
 
 
         }
