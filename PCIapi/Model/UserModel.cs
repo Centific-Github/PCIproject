01d0af811f6e880e95e4754f9fc13ff7915ac52a
jyothi121999 <jyothi.annabathina@pacteraedge.com>
2023-01-10 15:51:11 +0530

    Made Modifications in MangeLogin

--- PCIapi/Model/UserModel.cs
+++ PCIapi/Model/UserModel.cs
@@ -6,6 +6,8 @@ namespace PCIapi.Model
     public class UserModel
     {
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string EmailId { get; set; }
         public string Password { get; set; }
         public bool IsReset { get; set; }
