8caca0def25b93c2488e71417d51254cbd417927
Hema sri <hemasri.duvvuru@P0191367.com>
2023-01-16 20:08:41 +0530

    Created ManageProfile API

---
+++ PCIapi/Controllers/ManageProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PCIapi.Model;
+using System.Collections.Generic;
+
+namespace PCIapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageProfileController : ControllerBase
+    {
+
+        private IConfiguration _configuration;
+        private readonly ManageProfile manageProfile;
+        public ManageProfileController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            manageProfile = new ManageProfile(_configuration);
+        }
+        [HttpGet]
+        public IEnumerable<ManageProfileViewModel> Get()
+        {
+            return manageProfile.getProfileDetails();
+        }
+    }
+}
---
+++ PCIapi/Model/ManageProfile.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace PCIapi.Model
+{
+    public class ManageProfile : DBconnection
+    {
+        public ManageProfile(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+        public IEnumerable<ManageProfileViewModel> getProfileDetails()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT EmployeeID,Designation ,Department,JoinDate,EmailID,Status FROM MstUsers ";
+                dbConnection.Open();
+                return dbConnection.Query<ManageProfileViewModel>(sQuery);
+            }
+        }
+    
+
+    }
+
+public class ManageProfileViewModel
+    {
+        public string EmployeeID { get; set; }
+        public string Designation { get; set; }
+        public string  Department { get; set; }
+        public DateTime JoinDate { get; set; }
+        public string EmailID { get; set; }
+        public string Status { get; set; }
+    }
+}
+
